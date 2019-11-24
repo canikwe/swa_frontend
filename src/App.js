e4d540fe11be3de43a779fca73e65d4c38bc5a93
canikwe <c.anikwe@gmail.com>
2019-11-23 23:43:24 -0500

    add src/ directory for tracking

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { hot } from 'react-hot-loader'
+import '../index.css'
+
+
+const App = () => {
+  return (
+    <div className='App'>
+      <h1>The Fucking Weather</h1>
+    </div>
+  )
+}
+
+export default hot(module)(App)
