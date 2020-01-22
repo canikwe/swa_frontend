036ac9b7b80e66d681acba289c7084d2b0b53581
canikwe <c.anikwe@gmail.com>
2020-01-22 11:29:01 -0500

    change settings button to toggle and add styling

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -3,21 +3,28 @@ import React from 'react'
 const Settings = ({ saved, handleSave, scale, handleScale }) => {
   return (
     <div className='settings'>
-      <div>
+      <div className='scale'>
         <span 
-          className={scale === 'C' ? 'active' : ''}
+          className={scale === 'C' ? 'active' : 'inactive'}
           onClick={handleScale}
           id='C'
         >C°</span> | 
         <span 
-          className={scale === 'F' ? 'active' : ''}
+          className={scale === 'F' ? 'active' : 'inactive'}
           onClick={handleScale}
           id='F'
           name='F'
         > F°</span>
       </div>
-      <button onClick={ handleSave }>{ saved ? 'Remove' : 'Save' } my fucking settings</button>
-      {/* <input type='checkbox' checked={ checked } onChange={ handleChange } /> */}
+      <div className='settings'>
+        <label className='switch'>
+          <input type='checkbox' onChange={handleSave} checked={saved} />
+          <span className='slider round'></span>
+        </label>
+        <div>
+          {saved ? 'Remove' : 'Save'} my <br /> fucking settings
+        </div>
+      </div>
     </div>
   )
 }
