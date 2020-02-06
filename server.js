dbd1cb05b48bba291b65f90ae8b60beaeb195df1
canikwe <c.anikwe@gmail.com>
2020-02-06 11:57:50 -0500

    remove unused code in the server and add error and 404 handling

--- server.js
+++ server.js
@@ -1,21 +1,19 @@
-// import express from 'express'
-// import path from 'path'
-
 const express = require('express')
-const path = require('path')
 
 const port = process.env.PORT || 3001
 const app = express()
-const publicPath = path.join(__dirname, 'public')
 
-
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(publicPath, 'index.html'))
-//   })
-  
-// app.get('/', (req, res) => res.send('Hello from Express!'))
-
+// serving static files from the public directory
 app.use(express.static('public'))
+
+// handle 404 responses for routes outside '/' and '/index.html'
+app.use((req, res) => res.status(404).send("Sorry I can't find that!"))
+
+// error handling
+app.use((err, req, res) => {
+  console.log(err.stack)
+  res.status(500).send('Something broke!')
+})
   
 app.listen(port, () => {
   console.log(`server is up on ${port}!`)
