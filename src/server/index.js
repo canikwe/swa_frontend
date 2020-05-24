eb7be7fd4af3515e3445939712d67f8a79a59596
canikwe <c.anikwe@gmail.com>
2020-05-23 23:44:03 -0400

    remove public folder and files

--- src/server/index.js
+++ src/server/index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3001
 const app = express()
 
 // serving static files from the public directory
-app.use(express.static('public'))
+app.use(express.static('dist'))
 
 // handle 404 responses for routes outside '/' and '/index.html'
 app.use((req, res) => res.status(404).send("Sorry I can't find that!"))
