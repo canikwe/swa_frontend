7a8077b4424ec5dd268d61c1a67d83cdf2a1af11
Chine Anikwe <c.anikwe@gmail.com>
2022-02-14 03:44:30 -0500

    update packages, fixed infinite spinning, and ran prettier on a few files

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,26 +1,49 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const SearchBar = ({ searchTerm, handleBackNav, handleChange, handleSearch }) => {
+const SearchBar = ({ searchTerm, onNavigateBack, onChange, onSearch }) => {
+  const [errorMessage, updateErrorMessage] = useState(undefined);
 
-  return(
-    <div className='header animated fadeIn'>
-      <i onClick={handleBackNav} className="btn material-icons">arrow_back_ios</i>
-      <form className='header'>
-        <label htmlFor='location' />
-        <input
-          className='search'
-          type='search'
-          value={searchTerm} 
-          onChange={ handleChange } 
-          name='location' 
-          placeholder='Update your fucking location'
-        />
-        <button type='submit' onClick={handleSearch} className='btn'>
+  const validateSearch = (e) => {
+    e.preventDefault();
+    const strippedTerm = searchTerm.replace(/[^\w\s]/gi, "");
+
+    if (!strippedTerm) {
+      updateErrorMessage("That's not a fucking location");
+    } else {
+      updateErrorMessage(undefined);
+      onSearch(e);
+    }
+  };
+
+  const doSomething = (e) => {
+    if (errorMessage) updateErrorMessage(undefined);
+    onChange(e);
+  };
+
+  return (
+    <div className="header animated fadeIn">
+      <i onClick={onNavigateBack} className="btn material-icons">
+        arrow_back_ios
+      </i>
+      <form className="header">
+        <div>
+          <label htmlFor="location" />
+          <input
+            className="search"
+            type="search"
+            value={searchTerm}
+            onChange={doSomething}
+            name="location"
+            placeholder="Update your fucking location"
+          />
+        </div>
+        <button type="submit" onClick={validateSearch} className="btn">
           <i className="material-icons">search</i>
         </button>
       </form>
+      {errorMessage && <p className="danger">{errorMessage}</p>}
     </div>
-  )
-}
+  );
+};
 
-export default SearchBar
+export default SearchBar;
